1f5516f193e02d86a8141d9f46950f3c62d10875
antonyyxp <antoniomuniz2704@gmail.com>
2025-11-05 14:10:56 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String []args){
         System.out.println("testando pra ver se da certo essa bosta");
+        System.out.println("lava essa boca ai");
     }
 }
